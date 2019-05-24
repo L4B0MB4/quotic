e7836370546afea2b8bb23497719f2491293765f
L4B0MB4 <metalfan65@gmail.com>
2019-05-24 22:50:28 +0200

    added error message if args not present

--- index.js
+++ index.js
@@ -14,24 +14,36 @@ const centerHalfHeight = Math.abs((config.imageWidth - config.imageHeight) / 2);
 const centerHalfWidth = Math.abs((config.imageHeight - config.imageWidth) / 2);
 
 const args = process.argv;
-start(args[2], args[3]);
 
-async function start(title, citation) {
+if (args.length < 4) {
+  console.log("Please provide your title and citation as commandline arguments");
+  console.log("node index.js 'my title' 'my quote'");
+  return;
+} else {
+  start(args[2], args[3]);
+}
+
+async function start(title, quote) {
   const fontLarge = await jimp.loadFont(path.join(__dirname, "assets/font.fnt"));
-  let res = await jimp.read(config.inputFile);
-  await res.resize(config.imageWidth, config.imageWidth);
-  let res2 = await res.clone();
-  const background = await handleBackground(res2);
-  const foreground = await handleForeground(res);
+  let foreground = await jimp.read(config.inputFile);
+  await foreground.resize(config.imageWidth, config.imageWidth);
+  let background = await foreground.clone();
+  background = await handleBackground(background);
+  foreground = await handleForeground(foreground);
   background.composite(foreground, 0, centerHalfHeight);
   await background.brightness(config.brightness);
   await background.gaussian(config.gaussianTogether);
+  await addQuotationAndTitle(background, fontLarge, title, quote);
+  background.write(config.outputDir + title.replace(/ /g, "").toLowerCase() + ".png");
+}
+
+async function addQuotationAndTitle(background, fontLarge, title, quote) {
   await background.print(
     fontLarge,
     0,
     0,
     {
-      text: citation,
+      text: quote,
       alignmentX: jimp.HORIZONTAL_ALIGN_CENTER,
       alignmentY: jimp.VERTICAL_ALIGN_MIDDLE
     },
@@ -51,7 +63,6 @@ async function start(title, citation) {
     config.imageWidth,
     100
   );
-  background.write(config.outputDir + title.replace(/ /g, "").toLowerCase() + ".png");
 }
 
 async function handleBackground(image) {
